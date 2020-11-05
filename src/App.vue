6579c991a7c338abd100a4a8d24a97dcb6f9ca09
Ariunbold Davaasuren <d_ariunbold80@yahoo.com>
2020-11-06 00:28:07 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,30 @@
 <template>
-  <div id="app">
-    <Header />
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      flat
+    >
+     <Header/>
+    </v-app-bar>
+
+    <v-main>
+     
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Header from './components/layout/Header';
-
+import Header from './components/Header';
 
 export default {
-  name: "app",
+  name: 'App',
+
   components: {
     Header,
-  }
-}
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 10px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
