1bab4a17d4d0353060bcf48edae9ae4e709e88de
Ariunbold Davaasuren <d_ariunbold80@yahoo.com>
2020-11-10 19:27:13 +0100

    changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,92 +1,9 @@
 <template>
   <v-container fluid>
+    <div>
+      <Details />
+    </div>
 
-    <v-data-iterator
-      :items="items"
-      :items-per-page.sync="itemsPerPage"
-      hide-default-footer
-    >
-      <template v-slot:header>
-        <v-toolbar
-          class="mb-15"
-          color="indigo darken-5"
-          dark
-          flat
-        >
-          <router-link :to="{name:'Home'}">
-              <v-btn text>Home</v-btn>
-          </router-link>
-          <router-link :to="{name:'Movie'}">
-              <v-btn text>Categories</v-btn>
-          </router-link>
-          <router-link :to="{name:'Randomizer'}">
-              <v-btn text>Random</v-btn>
-          </router-link>
-
-        </v-toolbar>
-      </template>
-
-      <template v-slot:default="props">
-
-            <v-layout row class="pa-8 mb-2">
-              <v-btn small flat color="grey" @click="sortByAZ('original_title')">
-                  <v-icon left small></v-icon>
-                  <span class="text-lowercase">By movie title</span>
-              </v-btn>
-              <v-btn small flat color="grey" @click="sortBy('release_date')">
-                <v-icon left small></v-icon>
-                <span class="text-lowercase">By release date</span>
-              </v-btn>
-              <v-btn small flat color="grey" @click="sortBy('popularity')">
-                <v-icon left small></v-icon>
-                <span class="text-lowercase">By popularity</span>
-              </v-btn>
-              <v-btn small flat color="grey" @click="sortBy('vote_count')">
-                <v-icon left small></v-icon>
-                <span class="text-lowercase">By votes</span>
-              </v-btn>
-
-
-            </v-layout>
-
-
-            <v-card v-for="item in props.items" :key="item.name" class="pa-3">
-              <v-layout row wrap class="cards">
-                  <v-flex xs12 md6 >
-                      <div class="chip">
-                         <router-link :to="{name: 'Movie', params: {id: movie_id}}" x-large class="my-5 movie_titles">{{ item.original_title }}</router-link>
-                      </div>
-                  </v-flex>
-                  <v-flex xs6 sm4 md2>
-                    <div>Release date:</div>
-                    <div>{{ item.release_date }}</div>
-                  </v-flex>
-                  <v-flex xs6 sm4 md2>
-                    <div>Popularity:</div>
-                    <div>{{ item.popularity }}</div>
-                  </v-flex>
-                  <v-flex xs2 sm4 md2>
-                    <div>Votes:</div>
-                    <div>{{ item.vote_count }}</div>
-                  </v-flex>
-                </v-layout>
-            </v-card>
-      </template>
-
-      <template v-slot:footer>
-        <v-toolbar
-          class="mt-15"
-          color="indigo"
-          dark
-          flat
-        >
-          <v-toolbar-title class="subheading">
-            This is a footer
-          </v-toolbar-title>
-        </v-toolbar>
-      </template>
-
-    </v-data-iterator>
   </v-container>
 </template>
 
@@ -94,10 +11,16 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Details from '@/components/Details.vue'
+
 
 Vue.use(VueAxios,axios)
 
 export default {
+  name: 'home',
+  components: {
+    Details
+  },
     data: () => ({
         itemsPerPage: 10,
         items: [],
