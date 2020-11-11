1b388d78f89996d8f5dd5bb53e6abdfed9bd8597
Ariunbold Davaasuren <d_ariunbold80@yahoo.com>
2020-11-11 19:00:16 +0100

    changes

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,19 +1,20 @@
 <template>
-<div>     
-    <h3>Hello this is details.vu</h3>
+    <div>     
+        <h3>Hello this is details.vu</h3>
 
-    <p>{{ this.$route.params.id }}</p>
-
-
-    <ul>
-      <li v-for="mov in movie" :key="mov.title">
-        {{mov.title}}
-        {{mov.overview}}
-      </li>
-    </ul>
+        <p>{{ this.$route.params.id }}</p>
 
 
-</div>    
+        <ul>
+        <li v-for="mov in movie" :key="mov.title">
+            {{mov.title}}
+            {{mov.overview}}
+        </li>
+        </ul>
+
+        <img :src="url" alt="">
+
+    </div>    
 </template>
 
 <script>
@@ -26,19 +27,29 @@ export default {
     data() {
      return {
         movie: [],
+        url: '',
+        size: 'w300',
     }},
+
     mounted() {
         this.getDetails()
     },
     methods: {
+
         getDetails: function(){
 
             var movieId = this.$route.params.id
 
-            axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${secret_key}`).then((result) => {
-                console.log(result);
-                this.movie = result; 
-                })
+            axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${secret_key}`)
+            
+                .then((result) => {
+                    console.log(result);
+                    this.movie = result; 
+
+                    this.url = `https://image.tmdb.org/t/p/${this.size}/${result.data.poster_path}`
+
+                    console.log(this.url)
+                    })
         }
     }
 }
